ef54d64fee983792f42c57522122f6e8445614c3
Tw1ZZLER <ilekbrownies@gmail.com>
2024-03-19 14:15:13 -0400

    Update Item.java

--- src/wagonLoad/Item.java
+++ src/wagonLoad/Item.java
@@ -1,6 +1,26 @@
 package wagonLoad;
 
 public class Item {
-// this is the best class EVER!!!
-	//hi
+	private static int totalWeight = 0;
+	private int itemWeight = 0;
+	private int cost = 0;
+	
+	public Item() {
+	}
+	
+	public Item(int itemWeight) {
+		this.itemWeight = itemWeight;
+	}
+	
+	public int getItemWeight() {
+		return this.itemWeight;
+	}
+	
+	public void setItemWeight(int itemWeight) {
+		this.itemWeight = itemWeight;
+	}
+	
+	public static int getTotalWeight() {
+		return totalWeight;
+	}
 }
