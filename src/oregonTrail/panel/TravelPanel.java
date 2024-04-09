53327bc7b725895c7e9e3b7350e64f66c922bd3c
Tw1ZZLER <ilekbrownies@gmail.com>
2024-04-08 22:48:32 -0400

    Update TravelPanel.java

--- src/oregonTrail/panel/TravelPanel.java
+++ src/oregonTrail/panel/TravelPanel.java
@@ -5,6 +5,9 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Color;
 import javax.swing.SpringLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TravelPanel extends JPanel {
 
@@ -17,67 +20,67 @@ public class TravelPanel extends JPanel {
 		SpringLayout springLayout = new SpringLayout();
 		setLayout(springLayout);
 		
-		JLabel lblPressEnterTo = new JLabel("Press ENTER to size up the situation...");
-		springLayout.putConstraint(SpringLayout.NORTH, lblPressEnterTo, 137, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, lblPressEnterTo, 0, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblPressEnterTo, 167, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, lblPressEnterTo, 450, SpringLayout.WEST, this);
-		lblPressEnterTo.setOpaque(true);
-		lblPressEnterTo.setBackground(Color.BLACK);
-		lblPressEnterTo.setForeground(Color.WHITE);
-		lblPressEnterTo.setHorizontalAlignment(SwingConstants.CENTER);
-		this.add(lblPressEnterTo);
-		
-		JLabel lblPressSpaceTo = new JLabel("Press SPACE BAR to continue");
-		springLayout.putConstraint(SpringLayout.NORTH, lblPressSpaceTo, -24, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.WEST, lblPressSpaceTo, 0, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblPressSpaceTo, 0, SpringLayout.SOUTH, this);
-		springLayout.putConstraint(SpringLayout.EAST, lblPressSpaceTo, 0, SpringLayout.EAST, this);
-		lblPressSpaceTo.setOpaque(true);
-		lblPressSpaceTo.setHorizontalAlignment(SwingConstants.CENTER);
-		lblPressSpaceTo.setForeground(Color.WHITE);
-		lblPressSpaceTo.setBackground(Color.BLACK);
-		add(lblPressSpaceTo);
-		
 		JLabel lblDate = new JLabel("Date:");
-		springLayout.putConstraint(SpringLayout.NORTH, lblDate, 1, SpringLayout.SOUTH, lblPressEnterTo);
+		springLayout.putConstraint(SpringLayout.NORTH, lblDate, 168, SpringLayout.NORTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, lblDate, 74, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.EAST, lblDate, 186, SpringLayout.WEST, this);
 		lblDate.setHorizontalAlignment(SwingConstants.RIGHT);
 		add(lblDate);
 		
-		JLabel lbl = new JLabel("Weather:");
-		springLayout.putConstraint(SpringLayout.NORTH, lbl, 185, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblDate, 0, SpringLayout.NORTH, lbl);
-		springLayout.putConstraint(SpringLayout.EAST, lbl, 186, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.WEST, lbl, 74, SpringLayout.WEST, this);
-		lbl.setHorizontalAlignment(SwingConstants.RIGHT);
-		add(lbl);
+		JLabel lblWeather = new JLabel("Weather:");
+		springLayout.putConstraint(SpringLayout.SOUTH, lblDate, 0, SpringLayout.NORTH, lblWeather);
+		springLayout.putConstraint(SpringLayout.NORTH, lblWeather, 185, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, lblWeather, 74, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblWeather, 202, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, lblWeather, 186, SpringLayout.WEST, this);
+		lblWeather.setHorizontalAlignment(SwingConstants.RIGHT);
+		add(lblWeather);
 		
 		JLabel lblHealth = new JLabel("Health:");
 		springLayout.putConstraint(SpringLayout.NORTH, lblHealth, 202, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lbl, 0, SpringLayout.NORTH, lblHealth);
-		springLayout.putConstraint(SpringLayout.EAST, lblHealth, 186, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.WEST, lblHealth, 74, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblHealth, 219, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, lblHealth, 186, SpringLayout.WEST, this);
 		lblHealth.setHorizontalAlignment(SwingConstants.RIGHT);
 		add(lblHealth);
 		
 		JLabel lblFood = new JLabel("Food:");
 		springLayout.putConstraint(SpringLayout.NORTH, lblFood, 220, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblHealth, -1, SpringLayout.NORTH, lblFood);
-		springLayout.putConstraint(SpringLayout.EAST, lblFood, 186, SpringLayout.WEST, this);
 		springLayout.putConstraint(SpringLayout.WEST, lblFood, 74, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblFood, 237, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, lblFood, 186, SpringLayout.WEST, this);
 		lblFood.setHorizontalAlignment(SwingConstants.RIGHT);
 		add(lblFood);
 		
 		JLabel lblNextLandmark = new JLabel("Next Landmark:");
-		springLayout.putConstraint(SpringLayout.SOUTH, lblFood, -6, SpringLayout.NORTH, lblNextLandmark);
-		springLayout.putConstraint(SpringLayout.EAST, lblNextLandmark, 186, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.NORTH, lblNextLandmark, 0, SpringLayout.SOUTH, lblFood);
 		springLayout.putConstraint(SpringLayout.WEST, lblNextLandmark, 74, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.NORTH, lblNextLandmark, 243, SpringLayout.NORTH, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, lblNextLandmark, -16, SpringLayout.NORTH, lblPressSpaceTo);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNextLandmark, 254, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, lblNextLandmark, 0, SpringLayout.EAST, lblDate);
 		lblNextLandmark.setHorizontalAlignment(SwingConstants.RIGHT);
 		add(lblNextLandmark);
+		
+		JButton btnSizeUpThe = new JButton("Size Up the Situation");
+		springLayout.putConstraint(SpringLayout.NORTH, btnSizeUpThe, 133, SpringLayout.NORTH, this);
+		springLayout.putConstraint(SpringLayout.WEST, btnSizeUpThe, 0, SpringLayout.WEST, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnSizeUpThe, -140, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnSizeUpThe, 0, SpringLayout.EAST, this);
+		btnSizeUpThe.setBackground(Color.BLACK);
+		btnSizeUpThe.setForeground(Color.WHITE);
+		add(btnSizeUpThe);
+		
+		JButton btnContinue = new JButton("Continue");
+		springLayout.putConstraint(SpringLayout.NORTH, btnContinue, 9, SpringLayout.SOUTH, lblNextLandmark);
+		springLayout.putConstraint(SpringLayout.WEST, btnContinue, 0, SpringLayout.WEST, btnSizeUpThe);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnContinue, -10, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.EAST, btnContinue, 0, SpringLayout.EAST, btnSizeUpThe);
+		btnContinue.setForeground(Color.WHITE);
+		btnContinue.setBackground(Color.BLACK);
+		btnContinue.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		add(btnContinue);
 
 	}
 }
