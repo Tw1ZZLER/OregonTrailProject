0fd834d1f194d3d61d1d39502a832e46d41a6424
l-dunbar <143094474+l-dunbar@users.noreply.github.com>
2024-04-08 22:41:11 -0400

    Create LoadedWagonPanel.java

---
+++ src/oregonTrail/panel/LoadedWagonPanel.java
@@ -0,0 +1,16 @@
+package oregonTrail.panel;
+
+import javax.swing.JPanel;
+
+public class LoadedWagonPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public LoadedWagonPanel() {
+
+	}
+
+}
