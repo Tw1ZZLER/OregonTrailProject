bfaac9476b385ea3073bf9e79050e8fe05669afb
l-dunbar <143094474+l-dunbar@users.noreply.github.com>
2024-05-06 10:23:26 -0400

    Update Dialogue.java

--- src/oregonTrail/Dialogue.java
+++ src/oregonTrail/Dialogue.java
@@ -78,23 +78,23 @@ public class Dialogue {
 	    				+ "makes me want to set up camp here, its a shame its so wide open"
 	    				+ " though. I wouldn't make it a week!";
 	    	}
-	    	if(!Landmark.CHIMNEY_ROCK.isVisited()) {
+	    	else if(!Landmark.CHIMNEY_ROCK.isVisited()) {
 	    		message = "Well I'm sure glad we managed to stock up on water and supplies but this"
 	    				+ "walk across the desert will surely test our wits. I wish we’d still have some leftover mustard"
 	    				+ "greens from last night they sure were good.-Mrs. Kirkland";
 	    	}
-	    	if(!Landmark.THE_DALLES.isVisited()) {
+	    	else if(!Landmark.THE_DALLES.isVisited()) {
 	    		message = "These horses are the most stubborn and unruly animals I ever met, I can't"
 	    				+ "get the darn harness on em and we’re already late!. -Frank";
 	    	}
-	    	if(!Landmark.BLUE_MOUNTAINS.isVisited()) {
+	    	else if(!Landmark.BLUE_MOUNTAINS.isVisited()) {
 	    		message ="I can’t believe those damn herders drove away two of my best damn horses"
 	    				+ "and then still made you pay 2 dollars for your troubles absolutely ridiculous -Mr. Kirkland";
 	    	}
-	    	if(!Landmark.SODA_SPRINGS.isVisited()) {
-	    		message ="They stole my revolver!! Who in the hell stole it!! I swear I’ll kill that\r\n"
-	    				+ "rat!\"-John Smith, “Oh yeah with what gun (Laughter ensues from the crowd of soldiers)?!”- A\r\n"
-	    				+ "nameless Solider pg 173\r\n";
+	    	else if(!Landmark.SODA_SPRINGS.isVisited()) {
+	    		message ="They stole my revolver!! Who in the hell stole it!! I swear I’ll kill that"
+	    				+ "rat!\"-John Smith, “Oh yeah with what gun (Laughter ensues from the crowd of soldiers)?!”-"
+	    				+ "nameless Solider";
 	    	}
 	    	else {
 	    		message = "No one wanted to talk with you (Probably cause you stink)";
